b7bc0512ccc59948c61c840296c44f1c51ef9c39
Kevin A Collins <collins_kevin_a@lilly.com>
2024-02-29 14:59:16 -0500

    test commit

--- src/core.py
+++ src/core.py
@@ -1,12 +1,16 @@
 import helloworld.helloworld as sh
 from client import Client
-
+import os
 
 def make_client():
     m_client = Client()
     m_client.start_client()
     m_client.send_message("this is going to server")
 
+# def check_env_variables():
+#     env_variable_value = os.environm(["INITIAL_VALUE"])
+#     return float(env_variable_value)
 
 def say_hello():
+    # this has to be a comment
     return sh.hello_world()
